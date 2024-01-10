89bffe743435da65c549b621ffd37897a07c1cbc
Scottify20 <scottify20@gmail.com>
2024-01-10 20:03:40 +0800

    Added ellipsis truncation to card title

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const url = 'https://imdb8.p.rapidapi.com/auto-complete?q=game';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'ca86b98464msh7ca70580315d7f4p125960jsn4db09f3ab376',
+    'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
+  },
+};
 
+fetch(url, options)
+  .then((response) => {
+    const list = response.text();
+    return JSON.parse(list);
+  })
+  .then((list) => console.log(list))
+  .catch((err) => console.log(err));
+
+// const url = 'https://imdb8.p.rapidapi.com/actors/get-bio?nconst=nm0001667';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'ca86b98464msh7ca70580315d7f4p125960jsn4db09f3ab376',
+// 		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
