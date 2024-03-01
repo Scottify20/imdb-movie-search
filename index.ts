852a880d50a49983188e7b63c074a3b1849c5ada
Scottify20 <scottify20@gmail.com>
2024-03-01 18:31:35 +0800

    Added vercel web analytics

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import ButtonsDisabler from './src/utils/ButtonsDisabler';
 import { OmdbSearchLogic } from './src/components/header/search_bar/OmdbSearchLogic';
 import { navBarAutoHide } from './src/components/header/Header';
 import { mobileSidebarLogic } from './src/components/mobile_sidebar_menu/MobileSidebarMenu';
+import { inject } from '@vercel/analytics';
 
+inject(); // vercel analytics
 OmdbSearchLogic(true); //enables the search feature
 mobileSidebarLogic(true); // sidebar javascript logic for interactivity
 navBarAutoHide(true); // auto hiding and showing of navigation bar (header)
